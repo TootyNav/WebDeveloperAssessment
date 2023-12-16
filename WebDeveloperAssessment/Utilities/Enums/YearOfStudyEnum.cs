a5287afdeab7fca4dd798ff1b9e8c2d141a65f90
Navid Shiraj <navid2007@hotmail.com>
2023-12-16 23:07:36 +0000

    Add a new Enum to display student's year of study

---
+++ WebDeveloperAssessment/Utilities/Enums/YearOfStudyEnum.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDeveloperAssessment.Utilities.Enums
+{
+    public enum YearOfStudyEnum
+    {
+        [Display(Name = "1st")]
+        FirstYear,
+
+        [Display(Name = "2nd")]
+        SecondYear,
+
+        [Display(Name = "3rd")]
+        ThirdYear,
+        
+        [Display(Name = "4th")]
+        FourthYear
+    }
+}
