2890c5292a7664b0bd0525cdaceb9897ed0f99fd
Navid Shiraj <navid2007@hotmail.com>
2023-12-16 23:02:47 +0000

    Add Year of Study column to stdent entity

---
+++ WebDeveloperAssessment/Migrations/20231216225450_AddYearOfStudyToStudent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDeveloperAssessment.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddYearOfStudyToStudent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "YearOfStudy",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "1st");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "YearOfStudy",
+                table: "Student");
+        }
+    }
+}
--- WebDeveloperAssessment/Models/Student.cs
+++ WebDeveloperAssessment/Models/Student.cs
@@ -1,10 +1,15 @@
-﻿namespace WebDeveloperAssessment.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDeveloperAssessment.Models
 {
     public class Student
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public required int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
         public DateOnly Dob { get; set; }
+
+        [Display(Name = "Year of Study")]
+        public required string YearOfStudy { get; set; }
     }
 }
