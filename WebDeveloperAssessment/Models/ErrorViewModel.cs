26081d69c1e781b42495b24462393ca56552e8a7
Navid Shiraj <navid2007@hotmail.com>
2023-12-16 00:01:06 +0000

    Add project files.

---
+++ WebDeveloperAssessment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebDeveloperAssessment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
