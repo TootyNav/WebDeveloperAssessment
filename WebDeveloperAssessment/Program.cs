5894e6af26ff6bcd674d09d94a6994f6abf853a1
Navid Shiraj <navid2007@hotmail.com>
2023-12-16 20:09:33 +0000

    add Razor.RuntimeCompilation

--- WebDeveloperAssessment/Program.cs
+++ WebDeveloperAssessment/Program.cs
@@ -1,20 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using WebDeveloperAssessment.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<WebDeveloperAssessmentContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("WebDeveloperAssessmentContext") ?? throw new InvalidOperationException("Connection string 'WebDeveloperAssessmentContext' not found.")));
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
