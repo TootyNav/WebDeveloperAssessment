4d05e41962d93c523cff1a3c7dde6a1ec106e59c
Navid Shiraj <navid2007@hotmail.com>
2023-12-16 23:20:49 +0000

    add add a enum extension to get display name

---
+++ WebDeveloperAssessment/Utilities/Extensions/EnumGetDisplayName.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDeveloperAssessment.Utilities.Extensions
+{
+    public static class EnumGetDisplayName
+    {
+        public static string GetDisplayName(this Enum target)
+        {
+            var attributes = target.GetType().GetMember(target.ToString())
+                .First()
+                .GetCustomAttributes(typeof(DisplayAttribute), false)
+                .Cast<DisplayAttribute>();
+
+            foreach (var attribute in attributes)
+                return attribute.GetName();
+
+            return target.ToString();
+        }
+    }
+}
