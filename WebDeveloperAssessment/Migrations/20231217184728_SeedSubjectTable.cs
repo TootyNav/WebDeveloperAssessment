e66e545e946e36ac99b6c1db59c00665c80628d5
Navid Shiraj <navid2007@hotmail.com>
2023-12-17 18:48:58 +0000

    seed subject table with migration

---
+++ WebDeveloperAssessment/Migrations/20231217184728_SeedSubjectTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDeveloperAssessment.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSubjectTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                BULK INSERT dbo.Subject
+                FROM 'C:\Users\navid\Downloads\Confidential-WebDeveloperAssessment\Subjects.csv'
+                WITH
+                (
+                        FORMAT='CSV',
+                        FIRSTROW=2
+                )
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
