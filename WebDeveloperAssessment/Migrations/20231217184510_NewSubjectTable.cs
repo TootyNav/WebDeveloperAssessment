8c6df05af68f6516b289597b2233c8b81cad4f2d
Navid Shiraj <navid2007@hotmail.com>
2023-12-17 18:47:00 +0000

    add new table subjects  and migrations

---
+++ WebDeveloperAssessment/Migrations/20231217184510_NewSubjectTable.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDeveloperAssessment.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewSubjectTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SubjectId",
+                table: "Student",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Subject",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subject", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Student_SubjectId",
+                table: "Student",
+                column: "SubjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Subject_SubjectId",
+                table: "Student",
+                column: "SubjectId",
+                principalTable: "Subject",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Subject_SubjectId",
+                table: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Subject");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Student_SubjectId",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "SubjectId",
+                table: "Student");
+        }
+    }
+}
--- WebDeveloperAssessment/Models/Subject.cs
+++ WebDeveloperAssessment/Models/Subject.cs
@@ -4,6 +4,6 @@
     {
         public required int Id { get; set; }
         public required string Name { get; set; }
-        //public ICollection<Student>? Students { get; set; }
+        public ICollection<Student>? Students { get; set; }
     }
 }
